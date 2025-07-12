01ee4e01788e1ec0689fa2b7a462db849b151e5d
Jirusan Maonamprai <jirusan1307@gmail.com>
2025-07-12 15:29:16 +0700

    Add delete_todo function

--- todo.py
+++ todo.py
@@ -5,4 +5,9 @@ def show_todo():
     for i, task in enumerate(todo_list, 1):
         print(f"{i}. {task}")
 
+def delete_todo(index):
+    if 0 <= index < len(todo_list):
+        del todo_list[index]
+
+
 show_todo()
